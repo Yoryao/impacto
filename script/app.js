6771413a97d9b0c19c7976b56b48fa2a9adf4a07
Jorge Rivera <jrivera@VESSELARG.local>
2023-05-01 17:05:18 -0300

    agregado de la section cliente

--- script/app.js
+++ script/app.js
@@ -11,3 +11,5 @@ let presionar = (e) => {
 botonEnviar.addEventListener("click", presionar);
 
 // fin boton formulario
+
+console.log("Se debe modificar la linea 56 en app.js.     background-position:+85dvw;");
