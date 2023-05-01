e2d94f83633fe7069a68ee385094b3270b0f78a4
Jorge Rivera <jrivera@VESSELARG.local>
2023-05-01 15:54:02 -0300

    boton enviar agregado

--- script/app.js
+++ script/app.js
@@ -1,1 +1,13 @@
 console.log("app.js vinculado")
+
+// comportamiento boton Formulario
+let botonEnviar = document.getElementById("formButton");
+
+let presionar = (e) => {
+    e.preventDefault();
+    console.log("Boton Presionado");
+}
+
+botonEnviar.addEventListener("click", presionar);
+
+// fin boton formulario
