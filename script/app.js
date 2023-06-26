7f079499f52bad24e33db590bfeededc753de7e8
Tomas Lovato <lovato.tomas94@gmail.com>
2023-06-26 15:29:39 -0300

    version mobile y imagenes en carousel

--- script/app.js
+++ script/app.js
@@ -19,3 +19,19 @@ document.getElementById('form')
       alert(JSON.stringify(err));
     });
 });
+//hidden animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((element) => observer.observe(element));
+
+
